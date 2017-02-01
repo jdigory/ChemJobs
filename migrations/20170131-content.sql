c2cbb53df06d703d8e98978c0587e2c15de5ff02
Josh Lavin <digory@cpan.org>
2017-01-31 20:33:58 -0800

    add content migration

---
+++ migrations/20170131-content.sql
@@ -0,0 +1,10 @@
+UPDATE content SET category = NULL WHERE category = '';
+
+ALTER TABLE content ADD page_title VARCHAR(255) AFTER page_text;
+
+ALTER TABLE content ADD page_banner VARCHAR(255) AFTER page_title;
+
+UPDATE content SET
+    page_title = 'Technical recruiting for the chemical, refining &amp; gas processing industries',
+    page_banner = 'Technical recruiting for the chemical, refining &amp; gas processing industries'
+WHERE code = 'index';
